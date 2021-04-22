0a982cc5a4d46215783745e17fd632d0ed85ba85
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2021-04-22 20:34:39 -0300

    add models and parse operacao

---
+++ minhacarteira/Models.fs
@@ -0,0 +1,50 @@
+module MinhaCarteira.Models
+
+open System
+
+type TipoAtivo =
+    | Acao
+    | ETF
+    | BDR
+    | FII
+    | RendaFixa
+    | Poupanca
+    | Outro
+
+type Ativo = { Ticker: string; Tipo: TipoAtivo }
+
+type CarteiraAtivo =
+    { Ativo: string
+      Aplicado: decimal
+      PrecoMedio: decimal
+      Quantidade: int
+      Cotacao: decimal option
+      Patrimonio: decimal
+      PercentValorAplicado: decimal
+      PercentValorPatrimonio: decimal }
+
+type Carteira =
+    { Nome: string
+      TotalAplicado: decimal
+      TotalPatrimonio: decimal
+      Ativos: seq<CarteiraAtivo> }
+
+type Operacao =
+    { DtNegociacao: DateTime
+      Conta: int
+      Ativo: string
+      Preco: decimal
+      QuantidadeCompra: int
+      QuantidadeVenda: int }
+
+    member x.FinanceiroCompra = x.Preco * decimal x.QuantidadeCompra
+    member x.FinanceiroVenda = x.Preco * decimal x.QuantidadeVenda
+    override x.ToString() = x.Ativo
+
+type Posicao =
+    { Ativo: string
+      PrecoMedio: decimal
+      Quantidade: int }
+
+    member x.FinanceiroCompra = x.PrecoMedio * decimal x.Quantidade
+    override x.ToString() = $"{x.Quantidade} {x.Ativo} {x.PrecoMedio}"
---
+++ minhacarteira/ParserOperacao.fs
@@ -0,0 +1,46 @@
+module MinhaCarteira.Models.ParserOperacao
+
+open System.IO
+open System
+open MinhaCarteira.Models
+
+let parseLine culture (line: string) =
+    let columns = line.Replace(';', '\t').Split('\t')
+    let result = 
+        try 
+          let operation = {
+             DtNegociacao = DateTime.Parse(columns.[0], culture)
+             Conta = Int32.Parse( columns.[1], culture)
+             Ativo = columns.[2].Replace("\"", String.Empty).ToUpper()
+             Preco = Decimal.Parse(columns.[3], culture)
+             QuantidadeCompra = Int32.Parse(columns.[4], culture)
+             QuantidadeVenda = Int32.Parse(columns.[5], culture) 
+          }
+          Ok operation
+        with ex -> 
+          Error ex
+    Result.bind  
+      (fun operation -> 
+          if operation.QuantidadeCompra > 0 && operation.QuantidadeVenda > 0 then
+              Error <| Exception "não pode comprar e vender na mesma operação"
+          else
+              Ok operation) 
+      result
+
+let private split results = 
+    let bons, erros = ResizeArray<_>(), ResizeArray<_>()  
+    for item in results do
+      match item with
+      | Ok x -> bons.Add x
+      | Error x -> erros.Add x
+    bons :> seq<_>, erros :> seq<_> 
+
+let parseCSV filePath culture =
+    let lines = File.ReadAllLines(filePath)
+    let ops = 
+      lines
+         |> Array.skip 1
+         |> Array.where (String.IsNullOrWhiteSpace >> not)
+         |> Array.map (parseLine culture)
+         |> split
+    ops
