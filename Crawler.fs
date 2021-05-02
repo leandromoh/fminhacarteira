0a15cd5d27d0814d40c44f519a21bd23552e4b35
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2021-05-02 17:53:57 -0300

    added crawrler for fii and etf

--- Crawler.fs
+++ Crawler.fs
@@ -9,6 +9,28 @@ let private getBrowser() = async {
     return! Puppeteer.LaunchAsync(options) |> Async.AwaitTask  
 }
 
+let find url waitUntil script = async {  
+    use! browser = getBrowser()
+    use! page = browser.NewPageAsync() |> Async.AwaitTask  
+    let! _ = page.GoToAsync(url) |> Async.AwaitTask
+    let! _ = page.WaitForFunctionAsync(waitUntil) |> Async.AwaitTask
+    return! page
+            .EvaluateFunctionAsync<string[]>(script) 
+            |> Async.AwaitTask
+}  
+
+let getFIIs = 
+    let url = "https://fiis.com.br/lista-de-fundos-imobiliarios"
+    let waitUntil = "() => $('span.ticker').length > 290"
+    let script = "() => [...$('span.ticker')].map(x => x.innerHTML)"
+    find url waitUntil script
+
+let getETFs = 
+    let url = "https://br.investing.com/etfs/brazil-etfs"
+    let waitUntil = "() => $('#etfs td[title]').length > 40"
+    let script = "() => [...document.querySelectorAll('#etfs td[title]')].map(x => x.title)"
+    find url waitUntil script
+
 let getCotacao ativos = async {  
     use! browser = getBrowser()
     use! page = browser.NewPageAsync() |> Async.AwaitTask  
